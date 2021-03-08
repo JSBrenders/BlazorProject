a56de20542cb2f1fa6cc9dce488498bdde1d1ef9
Jean-Sébastien BRENDERS <j.brenders@epsi.fr>
2021-03-08 17:14:51 +0100

    Chess 0.2

--- wwwroot/js/Chess.js
+++ wwwroot/js/Chess.js
@@ -25,7 +25,7 @@ function LoadChessBoard() {
             var tile = document.createElement("div");
             if (i % 2 == 0) {
                 if (j % 2 == 0) {
-                    tile.classList.add("white")               
+                    tile.classList.add("white")
                 }
                 else {
                     tile.classList.add("black")
@@ -40,7 +40,7 @@ function LoadChessBoard() {
             }
 
             tile.classList.add("pieceContainer");
-          
+
             var numPiece = chessGame.initialState[i][j];
 
             var piece = document.createElement("div");
@@ -58,28 +58,78 @@ function LoadChessBoard() {
         }
 
 
-        board.appendChild(row);       
+        board.appendChild(row);
 
         $(".pieceContainer").droppable({
             //revert: true
             drop: function (event, ui) {
-                //event.target.firstChild.innerHTML == ui.draggable.innerHTML;
-                if (event.target.firstChild.innerHTML == "") {
+
+                resetIndications();
+
+                var dataDepart = $("#" + ui.draggable[0].id).data('position')
+                var dataArrivee = $("#" + event.target.firstChild.id).data('position')
+                var posDepart = [dataDepart.i, dataDepart.j];
+                var posArrivee = [dataArrivee.i, dataArrivee.j];
+
+
+                if (chessGame.checkPossibleTiles(dataDepart).contains(posArrivee)) {
                     event.target.firstChild.innerHTML = ui.draggable[0].innerHTML;
-                    //event.target.firstChild.innerHTML = "test";
-                    //console.log(event.target.firstChild.innerHTML);
                     ui.draggable[0].innerHTML = "";
+
+
+                    chessGame.currentState[posDepart[0]][posDepart[1]] = 0;
+                    chessGame.currentState[posArrivee[0]][posArrivee[0]] = dataDepart.p;
+
+                    chessGame.listePositions.push(chessGame.currentState);
+                    //chessGame.VisualizeState(chessGame.currentState, chessGame.currentTurn, chessGame.toWhite);
+                    chessGame.currentTurn++;
+                    chessGame.toWhite = !chessGame.toWhite;
+                    chessGame.toBlack = !chessGame.toBlack;
+
+
+                    //update front
+                    //update des datas des 2 tiles concernées
+                    $('#' + event.target.firstChild.id).data('position').p = $('#' + ui.draggable[0].id).data('position').p;
+                    $('#' + ui.draggable[0].id).data('position').p = 0;
+
+                    //on disable le drag and drop pour la couleur dont ce n'est pas le tour
+                    $(".piece").each(function () {
+                        if ($(this).data('position')) {
+
+                            var piece = $(this).data('position').p
+
+                            //si c'est au tour des blancs
+                            if (chessGame.toWhite) {
+                                //si la piece traitée est noire on disable sinon on enable
+                                if (chessGame.isBlack(piece)) {
+                                    //console.log("black disabled");
+                                    $(this).draggable('disable');
+                                } else if (chessGame.isWhite(piece)) {
+                                    //console.log("white enabled");
+                                    $(this).draggable({
+                                        helper: 'clone'
+                                    });
+                                    $(this).draggable('enable');
+                                }
+                            } else {
+                                //sinon c'est le tour des noirs et on inverse
+                                if (chessGame.isWhite(piece)) {
+                                    //console.log("white disabled");
+                                    $(this).draggable('disable');
+                                } else {
+                                    //console.log("black enabled");
+                                    $(this).draggable({
+                                        helper: 'clone'
+                                    });
+                                    $(this).draggable('enable');
+                                }
+                            }
+                        }
+                    });
+
                 }
             }
         });
-
-        $(".piece").draggable({
-            helper: 'clone',
-            //connectToSortable: ".pieceContainer",
-            //revert: "invalid"
-        }); 
-
-        
     }
 
     //ajout des lettres en bas
@@ -110,37 +160,71 @@ function LoadChessBoard() {
         var ligne = Math.trunc(i / 8, 0);
         var colonne = i % 8;
         $('#tile' + i).data('position', { i: ligne, j: colonne, p: chessGame.currentState[ligne][colonne] });
-        //console.log($('#tile' + i).data('position'));
         $('#tile' + i).click(function () {
-            console.log($(this).data('position'));
-            //var availablePositions = chessGame.checkPossibleTiles()
+            var data = $(this).data('position');
+            var availablePositions = chessGame.checkPossibleTiles(data);
+            //afficher les cases possibles
+            console.log("Positions disponibles : " + availablePositions);
+
+            //réinitialiser les cases
+            resetIndications();
+
+
+            for (var i = 0; i < availablePositions.length; i++) {
+
+                var x = availablePositions[i][0];
+                var y = availablePositions[i][1];
+
+
+                var tile = $('#tile' + (x * 8 + y));
+                var parent = $('#tile' + (x * 8 + y)).parent();
+
+                console.log(availablePositions[i]);
+                var tileJS = document.getElementById('tile' + (x * 8 + y));
+
+                var targetPiece = chessGame.currentState[x][y]
+
+                console.log("same couleur : " + chessGame.isSameColor(data.p, targetPiece));
+
+                if (data.p != 0 && (targetPiece == 0 || !chessGame.isSameColor(data.p, targetPiece))) {
+                    if (targetPiece == 0) {
+                        var previewElement = document.createElement("span");
+                        previewElement.classList.add("dot");
+                        parent.append(previewElement);
+                    } else {
+                        var targetElementOuter = document.createElement('div');
+                        var targetElementInner = document.createElement('div');
+                        targetElementOuter.classList.add('targetOuter');
+                        targetElementInner.classList.add('targetInner');
+                        targetElementInner.appendChild(tileJS);
+                        targetElementOuter.appendChild(targetElementInner);
+                        parent.append(targetElementOuter);
+                    }
+                }
+            }
         });
 
     }
-   
+
+    $(".piece").each(function () {
+        if ($(this).data('position').p < 7) {
+            $(this).draggable({
+                helper: 'clone'
+            });
+        }
+    });
+
 }
 
 
-
-//Fonction drag'n'drop
-function drop(ev) {
-    //ev.preventDefault();
-
-}
-
-function drag(ev) {
-    //ev.preventDefault();
-}
-
-function allowDrop(ev) {
-    //ev.preventDefault();
-}
 
 class ChessGame {
 
     constructor() {
 
         this.initialState = [];
+
+        this.listePositions = [this.initialState];
 
         //this.WR = ["R","white rook",3];
         //this.WN = ["N","white night",5];
@@ -172,6 +256,9 @@ class ChessGame {
 
         this.E = 0;
 
+        this.toWhite = true;
+        this.toBlack = false;
+
         //ajout des pièces noires
         this.initialState.push([this.BR, this.BN, this.BB, this.BQ, this.BK, this.BB, this.BN, this.BR])
         var whitePawnRow = []
@@ -198,58 +285,234 @@ class ChessGame {
         this.initialState.push([this.WR, this.WN, this.WB, this.WQ, this.WK, this.WB, this.WN, this.WR])
 
         this.currentState = this.initialState;
-
+        this.currentTurn = 0;
     }
 
 
     //On vérifie les cases possibles sur le board
-    checkPossibleTiles(numPiece) {
-
-        //on va renvoyer un tuple représentant une position
+    //data.i = ligne, data.j = colonne, data.p = numéro du type de piece
+    checkPossibleTiles(data) {
+        //on va renvoyer une liste d'objet pos = (ligne,colonne) de cases libres ou de pièces à prendre
         var freeTiles = [];
 
-        switch (numPiece) {
-            //roi blanc
+        var i = data.i;
+        var j = data.j;
+
+        switch (data.p) {
+            //roi blanc et roi noir
             case 1:
+            case 7:
+                for (var x = -1; x < 2; x++) {
+                    for (var y = -1; y < 2; y++) {
+                        if (i + x >= 0 && i + x < 8 && j + y >= 0 && j + y < 8) {
+                            if (this.currentState[i + x][j + y] == 0) {
+                                console.log("case traitée : " + (i + x) + "," + (j + y));
+                                freeTiles.push([i + x, j + y]);
+                            }
+                            else {
+                                var dataD = { i: i + x, j: j + y, p: this.currentState[i + x][j + y] }
+                                if (this.canTake(data, dataD)) {
+                                    var pos = [i + x, j + y]
+                                    freeTiles.push(pos);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                return freeTiles;
 
                 break;
-            //reine blanc
+            //reine blanche et reine noire
             case 2:
-                break;
-            //tour blanc
-            case 3:
-                break;
-            //fou blanc
-            case 4:
-                break;
-            //cavalier blanc
-            case 5:
-                break;
-            //pion blanc
-            case 6:
-                break;
-            //roi noir
-            case 7:
-                break;
-            //reine noir
             case 8:
+
+                //horizontal et vertical
+                //vertical - on procède case par case depuis la position de la piece
+                var direction = [1, -1];
+                for (var t = 0; t < 2; t++) {
+                    console.log(direction[t]);
+                    for (var x = direction[t]; x + i < 8 && x + i >= 0 ; x += direction[t]) {
+                        //console.log("i : " + x + " j : ", + y);
+                        console.log(x + i);
+                        if (this.currentState[x + i][j] == 0) {
+                            console.log("case traitée : " + x + i + "," + (j));
+                            freeTiles.push([x + i, j]);
+                        }
+                        else {
+                            var dataD = { i: x + i, j: j, p: this.currentState[x + i][j] }
+                            if (this.canTake(data, dataD)) {
+                                var pos = [x + i, j]
+                                freeTiles.push(pos);
+                            }
+                            break;
+                        }
+                    }
+                }
+                //if (i + x >= 0 && i + x < 8) {
+                //    if (this.currentState[i + x][j + y] == 0) {
+                //        console.log("case traitée : " + (i + x) + "," + (j + y));
+                //        freeTiles.push([i + x, j]);
+                //    }
+                //    else {
+                //        var dataD = { i: i + x, j: j, p: this.currentState[i + x][j + y] }
+                //        if (this.canTake(data, dataD)) {
+                //            var pos = [i + x, j]
+                //            freeTiles.push(pos);
+                //        }
+                //    }
+                //}
+
+
+                //diagonales
+
+
+                return freeTiles;
                 break;
-            //tour noir
+            //tour blanche et tour noir
+            case 3:
             case 9:
+                return freeTiles;
                 break;
-            //fou noir
+            //fou blanc et fou noir
+            case 4:
             case 10:
+                return freeTiles;
                 break;
-            //cavalier noir
+            //cavalier blanc et cavalier noir
+            case 5:
             case 11:
+                return freeTiles;
                 break;
-            //pion noir
+            //pion blanc et pion noir
+            case 6:
             case 12:
+                freeTiles.push([4, 4], [4, 5])
+                return freeTiles;
+                break;
+            ////roi noir
+            //case 7:
+            //    //on utilise le code du roi blanc
+            //    var data1 = data;
+            //    data1.p = 1;
+            //    return checkPossibleTiles(data1)
+            //    //return freeTiles;
+            //    break;
+            ////reine noire
+            //case 8:
+            //    return freeTiles;
+            //    break;
+            ////tour noire
+            //case 9:
+            //    return freeTiles;
+            //    break;
+            ////fou noir
+            //case 10:
+            //    return freeTiles;
+            //    break;
+            ////cavalier noir
+            //case 11:
+            //    return freeTiles;
+            //    break;
+            ////pion noir
+            //case 12:
+            //    freeTiles.push([6, 4])
+            //    return freeTiles;
+            //    break;
+            default:
                 break;
         }
     }
 
-    getPosition(numPiece) {
+    //si il y a un check
+    isCheked() {
 
     }
+
+
+    //peut prendre la piece adverse ou non
+    canTake(dataD, dataA) {
+        var posD = [dataD.i, dataD.j]
+        var posA = [dataA.i, dataA.j]
+
+        //si les pieces sont de couleur opposé
+        //if (dataA.p != 0 && this.isWhite(dataD.p) != this.isWhite(dataA.p)) {
+        //    //la piece visée est bien dans les cases possibles
+        //    //if (this.checkPossibleTiles(dataD).includes((dataD.i, dataD.j))) {
+        //        return true;
+        //    //}
+        //}
+        return !this.isSameColor(dataD.p, dataA.p);
+    }
+
+    //renvoi la couleur de la piece
+    isWhite(p) {
+        return p < 7 && p > 0;
+    }
+
+    isBlack(p) {
+        return p > 6 && p < 13;
+    }
+
+    isSameColor(p1, p2) {
+        return (this.isWhite(p1) && this.isWhite(p2)) || (this.isBlack(p1) && this.isBlack(p2));
+    }
+
+    //Visualiser un etat
+    VisualizeState(State, index, white) {
+
+        console.log("Position N°" + (index + 1) + " : " + (white ? "Trait aux blancs" : "Trait aux noirs"));
+
+        State.forEach(ligne => {
+            var row = "";
+            ligne.forEach(col => {
+                row += col + " ";
+            });
+            console.log(row);
+        });
+
+    }
+}
+
+
+
+
+Array.prototype.contains = function (subArray) {
+
+    var contain = false;
+
+    this.forEach(function (element) {
+        if (subArray.equals(element)) {
+            contain = true;
+        }
+    });
+
+
+    return contain;
+}
+
+Array.prototype.equals = function (Array2) {
+
+    var equals = true;
+
+    if (this.length != Array2.length) {
+        return false;
+    }
+
+    for (var i = 0; i < this.length; i++) {
+        if (this[i] != Array2[i]) {
+            equals = false;
+        }
+    }
+    return equals;
+}
+
+function resetIndications() {
+    $('.dot').remove();
+    $('.targetOuter').each(function () {
+        var piece = $(this).find('.piece');
+        var parent = $(this).parent();
+        $(parent).append(piece);
+    });
+    $('.targetOuter').remove();
 }
